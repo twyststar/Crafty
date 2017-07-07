ee4f355b9fe804605d17cc2c5504682501024ad5
Starlynne <lewis.start0905@gmail.com>
2017-07-07 11:18:28 -0700

    Add user show, add avatar and username to edit path, add user status method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def configure_permitted_parameters
     added_attrs = [:username, :email, :password, :password_confirmation, :remember_me, :avatar, :admin_role, :seller_role, :user_role, :current_password,]
       devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
- has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb =>   "100x100#" }, :default_url => ":style/missing.png"
- validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb =>   "100x100#" }, :default_url => ":style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
+  def status
+    if self.admin_role
+      "Admin"
+    elsif self.seller_role
+      "Seller"
+    else
+      "Member"
+    end
+  end
+
 end
