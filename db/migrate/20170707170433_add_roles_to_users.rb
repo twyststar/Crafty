d0be73e197cffb1b93570dccea5d5741e786d2c3
Starlynne <lewis.start0905@gmail.com>
2017-07-07 10:13:14 -0700

    Add roles and basic permission to users

---
+++ db/migrate/20170707170433_add_roles_to_users.rb
@@ -0,0 +1,7 @@
+class AddRolesToUsers < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :admin_role, :boolean
+    add_column :users, :seller_role, :boolean
+    add_column :users, :user_role, :boolean
+  end
+end
