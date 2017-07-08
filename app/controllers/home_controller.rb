6d0d6dfcb3a70d1d03e479cad72315cee074e902
Starlynne <lewis.start0905@gmail.com>
2017-07-07 18:43:07 -0700

    Add styling to users page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,11 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
+
+  end
+  def splash
+    if current_user
+      redirect_to home_index_path
+    end
   end
 end
